e5f9ac12f5dfe7a3091a6ec6edf2f60facc2b34a
Serega1067 <88957179+Serega1067@users.noreply.github.com>
2022-12-17 13:57:56 +0700

    Create databases_sql_homework5.sql
    Task seminar 5 done.

---
+++ databases_sql_homework5.sql
@@ -0,0 +1,223 @@
+-- Урок 5. SQL – оконные функции.
+
+-- Условие:
+
+CREATE SCHEMA databases_sql_homework5;
+
+USE databases_sql_homework5;
+
+create table users
+(
+    id  int auto_increment primary key,
+    login varchar(255) null,
+    pass  varchar(255) null,
+    male  tinyint      null
+);
+
+create table clients
+(
+    id    int auto_increment primary key,
+    login varchar(255) null,
+    pass  varchar(255) null,
+    male  tinyint      null
+);
+
+INSERT INTO users (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO users (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO users (login, pass, male) VALUES ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
+INSERT INTO users (login, pass, male) VALUES ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
+INSERT INTO users (login, pass, male) VALUES ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+INSERT INTO users (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+
+INSERT INTO clients (login, pass, male) VALUES ('alexander', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('Dmitry', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('Leonid', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Olga', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Masha', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+
+
+-- Задание на EXISTS
+CREATE TABLE Employee (
+  Id INT PRIMARY KEY,
+  Name VARCHAR(45) NOT NULL,
+  Department VARCHAR(45) NOT NULL,
+  Salary FLOAT NOT NULL,
+  Gender VARCHAR(45) NOT NULL,
+  Age INT NOT NULL,
+  City VARCHAR(45) NOT NULL
+);
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1001, 'John Doe', 'IT', 35000, 'Male', 25, 'London');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1002, 'Mary Smith', 'HR', 45000, 'Female', 27, 'London');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1003, 'James Brown', 'Finance', 50000, 'Male', 28, 'London');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1004, 'Mike Walker', 'Finance', 50000, 'Male', 28, 'London');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1005, 'Linda Jones', 'HR', 75000, 'Female', 26, 'London');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25, 'Mumbai');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1007, 'Priyanla Dewangan', 'HR', 45000, 'Female', 27, 'Mumbai');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28, 'Mumbai');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28, 'Mumbai');
+INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1010, 'Hina Sharma', 'HR', 75000, 'Female', 26, 'Mumbai');
+CREATE TABLE Projects (
+ ProjectId INT PRIMARY KEY AUTO_INCREMENT,
+      Title VARCHAR(200) NOT NULL,
+     ClientId INT,
+ EmployeeId INT,
+     StartDate DATETIME,
+     EndDate DATETIME
+);
+INSERT INTO Projects ( Title, ClientId, EmployeeId, StartDate, EndDate) VALUES 
+('Develop ecommerse website from scratch', 1, 1003, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY)),
+('WordPress website for our company', 1, 1002, NOW(), DATE_ADD(NOW(), INTERVAL 45 DAY)),
+('Manage our company servers', 2, 1007, NOW(), DATE_ADD(NOW(), INTERVAL 45 DAY)),
+('Hosting account is not working', 3, 1009, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY)),
+('MySQL database from my desktop application', 4, 1010, NOW(), DATE_ADD(NOW(), INTERVAL 15 DAY)),
+('Develop new WordPress plugin for my business website', 2, NULL, NOW(), DATE_ADD(NOW(), INTERVAL 10 DAY)),
+('Migrate web application and database to new server', 2, NULL, NOW(), DATE_ADD(NOW(), INTERVAL 5 DAY)),
+('Android Application development', 4, 1004, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY));
+
+-- Собеседования
+DROP TABLE AUTO;
+
+CREATE TABLE  AUTO 
+(       
+	REGNUM VARCHAR(10) PRIMARY KEY, 
+	MARK VARCHAR(10), 
+	COLOR VARCHAR(15),
+    PRICE DECIMAL,
+	RELEASEDT DATE, 
+	PHONENUM VARCHAR(15)
+);
+
+
+CREATE TABLE  CITY 
+(	
+    CITYCODE INT PRIMARY KEY,
+	CITYNAME VARCHAR(50), 
+	PEOPLES INT 
+);
+
+
+CREATE TABLE  MAN 
+(	
+	PHONENUM VARCHAR(15) PRIMARY KEY , 
+	FIRSTNAME VARCHAR(50),
+	LASTNAME VARCHAR(50),  
+	CITYCODE INT, 
+	YEAROLD INT	 
+);
+
+
+ -- AUTO
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, PRICE, RELEASEDT, PHONENUM ) VALUES
+(111114,'LADA', 'КРАСНЫЙ', 18927, date'2008-01-01', '9152222221'),
+(111115,'VOLVO', 'КРАСНЫЙ', 64324, date'2013-01-01', '9173333334'),
+(111116,'BMW', 'СИНИЙ', 132492, date'2015-01-01', '9173333334'),
+(111121,'AUDI', 'СИНИЙ', 55205, date'2009-01-01', '9173333332'),
+(111122,'AUDI', 'СИНИЙ', 45000, date'2011-01-01', '9213333336'),
+(111113,'BMW', 'ЗЕЛЕНЫЙ', 78345, date'2007-01-01', '9214444444'),
+(111126,'LADA', 'ЗЕЛЕНЫЙ', 14980, date'2005-01-01', null),
+(111117,'BMW', 'СИНИЙ', 96023, date'2005-01-01', null),
+(111119,'LADA', 'СИНИЙ', 16700, date'2017-01-01', 9213333331),
+(111120,'SKODA', 'ЗЕЛЕНЫЙ', 30927, date'2008-01-01', '9152222221'),
+(111118,'SKODA', 'ЧЕРНЫЙ', 29001, date'2013-01-01', '9173333334');
+
+ -- CITY
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(1,'Москва', 10000000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(2,'Владимир', 500000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(3, 'Орел', 300000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(4,'Курск', 200000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(5, 'Казань', 2000000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(7, 'Котлас', 110000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(8, 'Мурманск', 400000);
+
+
+INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
+VALUES(9, 'Ярославль', 500000);
+
+-- MAN
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9152222221','Андрей','Николаев', 1, 22);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9152222222','Максим','Москитов', 1, 31);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9153333333','Олег','Денисов', 3, 34);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9173333334','Алиса','Никина', 4, 31);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9173333335','Таня','Иванова', 4, 31);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9213333336','Алексей','Иванов', 7, 25);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9213333331','Андрей','Некрасов', 2, 27);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9213333332','Миша','Рогозин', 2, 21);
+
+
+INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
+VALUES('9214444444','Алексей','Галкин', 1, 38);
+
+-- Решение.
+-- 1. Создайте представление, в которое попадут автомобили стоимостью до 25 000 долларов.
+DROP VIEW auto_price;
+CREATE OR REPLACE VIEW auto_price AS
+	SELECT *
+    FROM AUTO
+    WHERE PRICE < 25000;
+SELECT *
+FROM auto_price;
+
+-- 2. Изменить в существующем представлении порог для стоимости: 
+-- пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW).
+ALTER VIEW auto_price AS
+	SELECT *
+    FROM AUTO
+    WHERE PRICE < 30000;
+SELECT *
+FROM auto_price;
+
+-- 3. Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди”.
+DROP VIEW auto_name;
+CREATE OR REPLACE VIEW auto_name AS
+	SELECT *
+    FROM AUTO
+    WHERE MARK = 'SKODA' OR MARK = 'AUDI';
+SELECT *
+FROM auto_name;
